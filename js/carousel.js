4d65cd85c450f20fc521c15b9d6892987c1a633f
nessrine-saggai <nina.nesbine2017@gmail.com>
2023-01-04 14:56:58 +0100

    change mobile slide number

--- js/carousel.js
+++ js/carousel.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
         }, {
             breakpoint:520,
             settings: {
-                slidesToShow: 3
+                slidesToShow: 2
             }
         }]
     });
